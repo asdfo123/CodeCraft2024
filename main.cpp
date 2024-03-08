e04fe5c0eeb1b769ca1095deb92780ca69531da0
asdfo123 <leeasdfo123@gmail.com>
2024-03-08 16:03:34 +0800

    Fix some bugs 还不能正常跑通BFS

--- main.cpp
+++ main.cpp
@@ -5,10 +5,7 @@ const int n = 200;
 const int robot_num = 10;
 const int berth_num = 10;
 const int N = 210;
-struct Point {
-    int x, y;
-    Point(int x, int y) : x(x), y(y) {}
-};
+
 struct Robot
 {
     int x, y, goods;
@@ -44,6 +41,7 @@ struct Boat
 struct Item{
     int x, y;
     int startzhen;
+    int val;
     bool operator==(const Item& other) const {
         return x == other.x && y == other.y && startzhen == other.startzhen;
     }
@@ -56,10 +54,6 @@ void Init()
 {
     for(int i = 1; i <= n; i ++)
         scanf("%s", ch[i] + 1);
-    for(int i = 1;i <= n;i++)
-        for(int j = 1;j <= n;j++)
-            if(ch[i][j] == 'A')
-                items.push_back(Item{i, j, 0});
     for(int i = 0; i < berth_num; i ++)
     {
         int idd;
@@ -82,6 +76,7 @@ int Input()
     {
         int x, y, val;
         scanf("%d%d%d", &x, &y, &val);
+        items.push_back(Item{x, y, id, val});
     }
     for(int i = 0; i < robot_num; i ++)
     {
@@ -97,11 +92,15 @@ int Input()
 
 const int INF = INT_MAX;
 
-
 // 0: right, 1: left, 2: up, 3: down
 const int dx[] = {0, 0, -1, 1};
 const int dy[] = {1, -1, 0, 0};
+struct Point {
+    int x, y;
+    Point(int x, int y) : x(x), y(y) {}
+};
 
+//BFS: return a 2D vector of shortest distance from start to each point.
 vector<vector<int>> bfs(const Point& start) {
     vector<vector<int>> dist(N, vector<int>(N, INF));
     queue<Point> q;
@@ -126,8 +125,9 @@ vector<vector<int>> bfs(const Point& start) {
     return dist;
 }
 
-int findNextMove(int robot_id, const vector<Item>& items ){
-    vector<vector<int>> dist = bfs(Point(robot[robot_id].x, robot[robot_id].y)); // Assuming the first robot position is used
+
+int findNextMove(int robot_id) {
+    vector<vector<int>> dist = bfs(Point(robot[robot_id].x, robot[robot_id].y));
     int min_distance = INF;
     Point targetPos(0, 0);
     for (const Item& item : items) {
@@ -143,11 +143,20 @@ int findNextMove(int robot_id, const vector<Item>& items ){
     int dir1 = -1;
     while (dist[x][y] > 1) {
         for (int dir = 0; dir < 4; dir++) {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
+            int nx = x - dx[dir];
+            int ny = y - dy[dir];
             if (nx >= 0 && nx < N && ny >= 0 && ny < N && dist[nx][ny] == dist[x][y] - 1) {
                 x = nx;
                 y = ny;
+                break;
+            }
+        }
+    }
+    if(dist[x][y] == 1){
+        for(int dir = 0; dir < 4; dir++){
+            int nx = x - dx[dir];
+            int ny = y - dy[dir];
+            if(nx == robot[robot_id].x && ny == robot[robot_id].y){
                 dir1 = dir;
                 break;
             }
@@ -166,13 +175,10 @@ void handle_item(int nowzhen)
 }
 void handle_robot(int robot_id)
 {
-    if(robot[robot_id].status == 0)
-        return;
-    vector<Point> item_positions;
-
-    int dir = findNextMove(robot_id, item_positions);
-    printf("%d\n", dir);
-    fflush(stdout);
+//    if(robot[robot_id].status == 0) //is_recovering
+//        return;
+//    int dir = findNextMove(robot_id);
+    printf("move %d %d\n", robot_id, rand()%4);
 }
 int main()
 {
@@ -180,10 +186,10 @@ int main()
     for(int zhen = 1; zhen <= 15000; zhen ++)
     {
         int id = Input();
-        handle_item(zhen)
-        for(int i = 0; i < robot_num; i++)
+        handle_item(zhen);
+        for(int i = 0; i < robot_num; i++){
             handle_robot(i);
-
+        }
         puts("OK");
         fflush(stdout);
     }
